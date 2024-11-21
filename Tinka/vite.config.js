71e375121b2727146a08acc9fa6eb67b3808ddca
kinglove58 <kprincejah@gmail.com>
2024-11-21 09:46:22 +0100

    chore: remove package.json and package-lock.json, add robots.txt for SEO

--- Tinka/vite.config.js
+++ Tinka/vite.config.js
@@ -2,34 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import viteCompression from "vite-plugin-compression";
-import { SitemapStream, streamToPromise } from "sitemap";
-import { createWriteStream } from "fs";
-
-function generateSitemap() {
-  return {
-    name: "generate-sitemap",
-    closeBundle: async () => {
-      const links = [
-        { url: "/", changefreq: "daily", priority: 0.9 },
-        { url: "/about", changefreq: "monthly", priority: 0.7 },
-        // Add more URLs here
-      ];
-
-      const sitemap = new SitemapStream({
-        hostname: "https://tinkahealthservices.com",
-      });
-
-      await streamToPromise(
-        sitemap.pipe(createWriteStream("./dist/sitemap.xml"))
-      )
-        .then(() => console.log("Sitemap created successfully"))
-        .catch((err) => console.error("Error creating sitemap", err));
-
-      links.forEach((link) => sitemap.write(link));
-      sitemap.end();
-    },
-  };
-}
 
 export default defineConfig({
   plugins: [
@@ -38,7 +10,6 @@ export default defineConfig({
     viteCompression({
       algorithm: "brotliCompress", // Use Brotli compression
     }),
-    generateSitemap(), // Add the sitemap generation plugin
   ],
   build: {
     rollupOptions: {
