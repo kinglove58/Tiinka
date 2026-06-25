91d62a98a5b020a50d6036e76360c621a929dfcf
kinglove58 <obafemielijahsunday@gmail.com>
2026-06-25 16:37:51 +0100

    Fix Sanity Studio deploy config

---
+++ sanity-studio/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  studioHost: "tinkahealthservices",
+  api: {
+    projectId: "bg7lu9zb",
+    dataset: "production",
+  },
+});
